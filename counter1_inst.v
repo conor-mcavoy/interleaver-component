04a6d9812b938db08b76d139aa3f28a8a8f702d5
Conor McAvoy <conorgmcavoy@gmail.com>
2020-03-03 19:45:50 -0500

    deleted some files

--- counter1_inst.v
+++
@@ -1,6 +0,0 @@
-counter1	counter1_inst (
-	.aclr ( aclr_sig ),
-	.clk_en ( clk_en_sig ),
-	.clock ( clock_sig ),
-	.q ( q_sig )
-	);
--- pi1_small_inst.v
+++
@@ -1,5 +0,0 @@
-pi1_small	pi1_small_inst (
-	.address ( address_sig ),
-	.clock ( clock_sig ),
-	.q ( q_sig )
-	);
